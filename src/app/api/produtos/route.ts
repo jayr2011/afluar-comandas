7dbc594861d79ff021c86f5328257815154fb8d1
Jair Costa <juniorceia1996@gmail.com>
2026-02-13 17:54:19 -0300

    Adicionar controller de produtos e corrigir importação no arquivo de rotas

--- src/app/api/produtos/route.ts
+++ src/app/api/produtos/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { ProdutosController } from '@/app/controller/productController'
+import { ProdutosController } from '@/controller/productController'
 
 /**
  * API routes for produtos.
